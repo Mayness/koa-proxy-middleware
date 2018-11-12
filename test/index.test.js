afe6e2080d962b8eb0184d34af53723b23338c90
邓茗予 <1095346833@qq.com>
2018-11-12 23:46:21 +0800

    fix: 修改单元测试

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,9 @@
 'use strict';
-const Proxy = require('../');
+const Proxy = require('..');
 const request = require('supertest');
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
-// const path = require('path');
+const path = require('path');
 const beforeAll = global.beforeAll;
 const afterAll = global.afterAll;
 const expect = global.expect;
@@ -26,7 +26,7 @@ beforeAll(() => {
         case '/upload': res = 'success'; break;
         case '/other/entry': res = ctx.request.body; break;
         case '/singleNoRewrite/entry': res = ctx.request.body; break;
-        case '/allNoRewrite2/entry': res = ctx.request.body; break;
+        case '/allNoRewrite2': res = ctx.request.body; break;
         default: break;
       }
     }
@@ -46,7 +46,7 @@ afterAll(() => {
   server.close();
 });
 
-describe('ngnix server is connection', () => {
+describe('nginx server is connection', () => {
   test('connection success', async done => {
     const res = await request(server).get('/');
     expect(res.body.status).toBe(200);
@@ -54,16 +54,16 @@ describe('ngnix server is connection', () => {
   });
 });
 
-describe('koa-ngnix in bodyparser Middleware test', () => {
-  let serverNgnix;
+describe('koa-nginx in bodyparser Middleware test', () => {
+  let serverNginx;
   let agent;
   beforeAll(() => {
     const app = new Koa();
-    const ngnix = Proxy.proxy({
+    const nginx = Proxy.proxy({
       proxies: [
         {
           host: 'http://localhost:3333/',
-          context: 'ngnix',
+          context: 'nginx',
         },
         {
           host: 'http://localhost:3333/',
@@ -93,16 +93,16 @@ describe('koa-ngnix in bodyparser Middleware test', () => {
       },
     });
     app.use(bodyParser());
-    app.use(ngnix);
-    serverNgnix = app.listen(4444);
-    agent = request.agent(serverNgnix);
+    app.use(nginx);
+    serverNginx = app.listen();
+    agent = request.agent(serverNginx);
   });
   afterAll(() => {
-    serverNgnix.close();
+    serverNginx.close();
   });
 
   test('GET method', async done => {
-    const res = await agent.get('/ngnix/getMethod')
+    const res = await agent.get('/nginx/getMethod')
       .set('newtoken', 'tokenPassword')
       .query({
         test: 2222,
@@ -113,7 +113,7 @@ describe('koa-ngnix in bodyparser Middleware test', () => {
   });
 
   test('POST method', async done => {
-    const res = await agent.post('/ngnix/bodyParse')
+    const res = await agent.post('/nginx/bodyParse')
       .set('Content-Type', 'application/json; charset=utf-8')
       .query({
         test2: 2222,
@@ -124,93 +124,92 @@ describe('koa-ngnix in bodyparser Middleware test', () => {
     done();
   });
 
-  //   test('upload test', async done => {
-  //     const res = await new Promise(resolve => {
-  //       agent.post('/ngnix/upload')
-  //         .set('Content-Type', 'multipart/form-data')
-  //         .field('name', 'testImg')
-  //         .attach('avatar', path.join(__dirname, './static/test.png'))
-  //         .end((err, result) => {
-  //           if (!err) {
-  //             resolve(result);
-  //           }
-  //         });
-  //     });
-  //     expect(res);
-  //     done();
-  //   });
+  test('upload test', async done => {
+    const res = await new Promise(resolve => {
+      agent.post('/nginx/upload')
+        .set('Content-Type', 'multipart/form-data')
+        .field('name', 'testImg')
+        .attach('avatar', path.join(__dirname, './static/test.png'))
+        .end((err, result) => {
+          if (!err) {
+            resolve(result);
+          }
+        });
+    });
+    expect(res);
+    done();
+  });
 
-  //   test('single rewrite test', async done => {
-  //     const res = await agent.post('/rewrite/entry')
-  //       .set('Content-Type', 'application/x-www-form-urlencoded')
-  //       .send({
-  //         test: 333,
-  //       });
-  //     expect(res.body.data.test).toBe('333');
-  //     done();
-  //   });
+  test('single rewrite test', async done => {
+    const res = await agent.post('/rewrite/entry')
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .send({
+        test: 333,
+      });
+    expect(res.body.data.test).toBe('333');
+    done();
+  });
 
-  //   test('rewrite false test', async done => {
-  //     const res = await agent.post('/singleNoRewrite/entry')
-  //       .set('Content-Type', 'application/x-www-form-urlencoded')
-  //       .send({
-  //         test: 444,
-  //       });
-  //     expect(res.body.data.test).toBe('444');
-  //     done();
-  //   });
+  test('rewrite false test', async done => {
+    const res = await agent.post('/singleNoRewrite/entry')
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .send({
+        test: 444,
+      });
+    expect(res.body.data.test).toBe('444');
+    done();
+  });
 
-//   test('error handle', async done => {
-//     const res = await agent.post('/notFound');
-//     expect(res.status).toBe(505);
-//     done();
-//   });
+  test('exact url match', async done => {
+    const res = await agent.get('/nginx123')
+      .query({
+        test: 2222,
+      });
+    expect(res.body.data).toBeUndefined();
+    done();
+  });
+
+  test('error handle', async done => {
+    const res = await agent.post('/notFound');
+    expect(res.status).toBe(505);
+    done();
+  });
 });
 
-// describe('all rewrite false test', () => {
-//   let serverNgnix;
-//   let agent;
-//   beforeAll(() => {
-//     const app = new Koa();
-//     const ngnix = proxy({
-//       rewrite: false,
-//       proxies: [
-//         {
-//           host: 'http://localhost:3333/',
-//           context: 'allNoRewrite1',
-//         },
-//         {
-//           host: 'http://localhost:3333/',
-//           context: 'allNoRewrite2',
-//         },
-//       ],
-//     });
-//     app.use(bodyParser());
-//     app.use(ngnix);
-//     serverNgnix = app.listen(5555);
-//     agent = request.agent(serverNgnix);
-//   });
+describe('all rewrite false test', () => {
+  let serverNginx;
+  let agent;
+  beforeAll(() => {
+    const app = new Koa();
+    const nginx = Proxy.proxy({
+      rewrite: false,
+      proxies: [
+        {
+          host: 'http://localhost:3333/',
+          context: 'allNoRewrite1',
+        },
+        {
+          host: 'http://localhost:3333/',
+          context: 'allNoRewrite2',
+        },
+      ],
+    });
+    app.use(bodyParser());
+    app.use(nginx);
+    serverNginx = app.listen(5555);
+    agent = request.agent(serverNginx);
+  });
 
-//   afterAll(() => {
-//     serverNgnix.close();
-//   });
+  afterAll(() => {
+    serverNginx.close();
+  });
 
-//   test('all no rewrite get test', async done => {
-//     const res = await agent.get('/allNoRewrite1')
-//       .query({
-//         test: 555,
-//       });
-//     expect(res.body.data.test).toBe('555');
-//     done();
-//   });
-
-//   test('all no rewrite post test', async done => {
-//     const res = await agent.post('/allNoRewrite2/entry')
-//       .set('Content-Type', 'application/x-www-form-urlencoded')
-//       .send({
-//         test: 666,
-//       });
-//     expect(res.body.data.test).toBe('666');
-//     done();
-//   });
-// })
+  test('all no rewrite get test', async done => {
+    const res = await agent.get('/allNoRewrite1')
+      .query({
+        test: 555,
+      });
+    expect(res.body.data.test).toBe('555');
+    done();
+  });
+});
